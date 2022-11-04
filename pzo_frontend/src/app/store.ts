d31589190ff68c38288b73ca859b73ad523e9fad
KapelanJR <kacperszmitk12@gmail.com>
2022-11-04 23:25:58 +0100

    React cleanup

--- pzo_frontend/src/app/store.ts
+++ pzo_frontend/src/app/store.ts
@@ -1,9 +1,9 @@
 import { configureStore, ThunkAction, Action } from '@reduxjs/toolkit';
-import counterReducer from '../features/counter/counterSlice';
+import rootReducer from "./reducers";
 
 export const store = configureStore({
   reducer: {
-    counter: counterReducer,
+    reducer: rootReducer,
   },
 });
 
