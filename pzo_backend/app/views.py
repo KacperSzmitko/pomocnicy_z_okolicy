cfcec25b437cc947ebebf73a30386442afca487a
Maciej Stefaniak <48411094+madragonse@users.noreply.github.com>
2022-11-05 00:56:40 +0100

    add user points, first

--- pzo_backend/app/views.py
+++ pzo_backend/app/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
+from rest_framework.views import APIView 
+from rest_framework.generics import UpdateAPIView 
+from rest_framework.serializers import ModelSerializer
+from rest_framework.response import Response
 
-# Create your views here.
+from pzo_backend.app.models import UsersData
+
+class UserRate(UpdateAPIView):
+    class ClientNameSerializer(ModelSerializer):
+        class Meta:
+            model = UsersData
+            fields = ("points",)
+
+    queryset = UsersData.objects.all()
+
+            
+    def update(self, request, *args, **kwargs):
+        instance:UsersData = UsersData.objects.get(user=request.user)
+        instance.points += request.data.get("points")
+        instance.save()
+
+        return Response(str(isinstance))
+
+    
--- pzo_backend/pzo/urls.py
+++ pzo_backend/pzo/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import path, include
+from app.views import UserRate
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("auth/", include("authorization.urls", "authorization")),
+    path("user/rate", UserRate.as_view()),
+
 ]
