6cf739dfd1f1efb67e9e26674ae2418b83a274ec
Remni1 <marcin.tajsner@gmail.com>
2022-11-04 23:37:44 +0100

    Database model

--- pzo_backend/app/models.py
+++ pzo_backend/app/models.py
@@ -1,3 +1,40 @@
+from datetime import datetime
 from django.db import models
+from authorization.models import User
+from django.core import validators
 
-# Create your models here.
+class UsersData(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    firstname = models.CharField(max_length=255)
+    surname = models.CharField(max_length=255)
+    age = models.IntegerField(validators=[validators.MinValueValidator(0)])
+    city = models.CharField(max_length=255)
+    points = models.IntegerField(default=0)
+
+class ReportTypes(models.Model):
+    type_name = models.CharField(max_length=63)
+    lifespan = models.IntegerField(default=3600)
+
+class ReportStates(models.Model):
+    state_name = models.CharField(max_length=63)
+
+class Reports(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    report_type = models.ForeignKey(ReportTypes, on_delete=models.CASCADE)
+    report_state = models.ForeignKey(ReportStates, on_delete=models.CASCADE)
+    latitude = models.FloatField()
+    altitude = models.FloatField()
+    time = models.DateTimeField()
+    max_people = models.IntegerField(validators=[
+        validators.MinValueValidator(1),
+        validators.MaxValueValidator(999)
+        ], null=True)
+    current_people = models.IntegerField(validators=[
+        validators.MinValueValidator(0),
+        validators.MaxValueValidator(999)
+        ], null=True)
+    description = models.CharField(max_length=255, null=True)
+
+class ReportTypesToAccept(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    report_type = models.ForeignKey(ReportTypes, on_delete=models.CASCADE)
