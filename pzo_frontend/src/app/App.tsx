8ab69d51e935212ddd753d7dd3e76514b99f4b62
KapelanJR <kacperszmitk12@gmail.com>
2022-11-05 10:19:58 +0100

    Data fetching v1

--- pzo_frontend/src/app/App.tsx
+++ pzo_frontend/src/app/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../common/HomePage";
 import LoginPage from "../features/auth/LoginPage";
+import Map from '../features/map/Map';
 
 function App() {
   return (
     <div className="app" id="main_window">
       <Routes>
         <Route path="/" element={<HomePage />}>
-          <Route path="/" element={<div></div>}></Route>
+          <Route path="/" element={<Map/>}></Route>
           <Route path="*" element={<div></div>}></Route>
         </Route>
         <Route path="login" element={<LoginPage />}></Route>
--- pzo_frontend/src/features/map/Map.tsx
+++ pzo_frontend/src/features/map/Map.tsx
@@ -1,9 +1,48 @@
-import React from 'react'
+import React from "react";
+import { GoogleMap, useJsApiLoader } from "@react-google-maps/api";
+
+const containerStyle = {
+  width: "400px",
+  height: "400px",
+};
+
+const center = {
+  lat: -3.745,
+  lng: -38.523,
+};
 
 function Map() {
-  return (
-    <div>Map</div>
-  )
+  const { isLoaded } = useJsApiLoader({
+    id: "google-map-script",
+    googleMapsApiKey: "AIzaSyCOv6Fj9IET_ieFKC5hQ359MqZnGDrgBKg",
+  });
+
+  const [map, setMap] = React.useState(null);
+
+  const onLoad = React.useCallback(function callback(map: any) {
+    const bounds = new window.google.maps.LatLngBounds(center);
+    map.fitBounds(bounds);
+    setMap(map);
+  }, []);
+
+  const onUnmount = React.useCallback(function callback(map: any) {
+    setMap(null);
+  }, []);
+
+  return isLoaded ? (
+    <GoogleMap
+      mapContainerStyle={containerStyle}
+      center={center}
+      zoom={10}
+      onLoad={onLoad}
+      onUnmount={onUnmount}
+    >
+      {/* Child components, such as markers, info windows, etc. */}
+      <></>
+    </GoogleMap>
+  ) : (
+    <></>
+  );
 }
 
-export default Map
+export default React.memo(Map);
