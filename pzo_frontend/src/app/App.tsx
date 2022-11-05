ae6f438fb874cef08f0170bef88fa43dd54ca67f
Remni1 <marcin.tajsner@gmail.com>
2022-11-05 12:04:56 +0100

    report create test

--- pzo_frontend/src/app/App.tsx
+++ pzo_frontend/src/app/App.tsx
@@ -2,7 +2,8 @@ import React from 'react';
 import { Routes, Route } from "react-router-dom";
 import HomePage from "../common/HomePage";
 import LoginPage from "../features/auth/LoginPage";
-import Notifications from '../features/responseReport/Notifications';
+import CreateReport from '../features/reports/CreateReport';
+import Map from '../features/map/Map';
 
 function App() {
   return (
@@ -13,7 +14,7 @@ function App() {
           <Route path="*" element={<div></div>}></Route>
         </Route>
         <Route path="login" element={<LoginPage />}></Route>
-        <Route path="send_report" element={<div></div>}></Route>
+        <Route path="create_report" element={<CreateReport />}></Route>
       </Routes>
     </div>
   );
